19feae1f5db14826778f4b582634b82e7abb684a
popsiclestand <goldfinger59@gmail.com>
2015-11-27 23:48:56 +0000

    includes final first project

--- FIRST_MAJOR_PROJECT_FINAL.cc
+++ FIRST_MAJOR_PROJECT_FINAL.cc
@@ -1,4 +1,6 @@
-// This program lets the user earn money while playing a math game
+// The Math Program lets users earn money while answering simple arithmetic problems
+
+//libraries being used
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -8,41 +10,41 @@
 #include <iomanip>
 #include <sstream>
 
+//setting namespace to be used
 using namespace std;
 
+//function prototype declarations
 void credits();
-string retrieveStats();
-string validateUserResponse(string userEntry);
-string validateUserResponse2(string userEntry);
-void displayStats(string displayStatsUser);
-void menu(string menuUser);
-void generateAddition(string userName);
-void generateDivision();
-void generateSubtraction();
-void generateMultiplication();
+void retrieveStats(string& userName);
+void validateUserResponse(string& userEntry);
+int validateUserResponse2(string userEntry); //this does not utilize pass by reference because the data type of the variable that needs to be returned is different than the data type of the parameter for the function
+void displayStats(string& userName);
+void menu(string& userName);
+void generateAddition(string& userName);
+void generateDivision(string& userName);
+void generateSubtraction(string& userName);
+void generateMultiplication(string& userName);
 void quit();
-void updateStats(string userName, bool adjustStat);
+void updateStats(string& userName, bool& status);
 
-//global constants
+//global constants declarations
 const double ADD_MONEY = 0.05;
 const double SUBTRACT_MONEY = 0.03;
 
-
+//main function that calls all of the other functions.
 int main()
 {
-    string name;
+    string name = "";
     credits();
     cout << "\n";
-    name = retrieveStats();
+    retrieveStats(name);
     displayStats(name);
-    //displayStats(retrieveStats());
-    cout << "\n\n\n";
     menu(name);
     
     return 0;
 }
 
-void credits()
+void credits()//prints the welcoming message for user and first instructions to enter user name
 {
     cout << "**************************************" << endl;
     cout << "***** WELCOME TO THE MATH GAME! ******" << endl;
@@ -55,23 +57,24 @@ void credits()
 }
 
 
-string retrieveStats()
+//this function opens files for users that exist and creates files for users that do not, populating that file with default data
+void retrieveStats(string& userName)
 {
-    string inputUserName = "";
-    string temp;
+    //string temp;
     ifstream inputUserNameInFile;
     ofstream outputUserNameOutFile;
-    int defaultData[3] = {0,0,0};
-    int counter = 0;
+    string correctAnswers = "0";
+    string wrongAnswers = "0";
+    string moneyEarned = "0.00";
     
     cout << ">>";
-    getline(cin,inputUserName);
+    getline(cin,userName);
     cout << "\n\n\n";
     
-    temp = validateUserResponse(inputUserName);
-    inputUserName = temp;
+    validateUserResponse(userName);
+    //userName = temp;
     
-    inputUserNameInFile.open((inputUserName + ".txt").c_str());
+    inputUserNameInFile.open((userName + ".txt").c_str());
     
     if (inputUserNameInFile)
     {
@@ -80,38 +83,32 @@ string retrieveStats()
     else
     {
         cout << "Your user name does not exist, but one shall be created for you." << endl;
-        cout << endl;
-        outputUserNameOutFile.open((inputUserName + ".txt").c_str());
         
-        for (counter = 0; counter <= 2 ; counter++)
-        {
-            outputUserNameOutFile << defaultData[counter] << "\n";
-        }
+        outputUserNameOutFile.open((userName + ".txt").c_str());
+
+        outputUserNameOutFile << correctAnswers << "\n" << wrongAnswers << "\n" << moneyEarned;
         
         outputUserNameOutFile.close();
     }
     
-    return inputUserName;
 }
 
-//user validation for string entries ONLY!! Checks for the use of special characters
-//could possibly add in the uppercase conversion as well
-string validateUserResponse(string userEntry)
+//module to validate the user's responses to check for spaces, and special characters
+void validateUserResponse(string& userEntry)
 {
-    string response = userEntry;
     int counter = 0;
     int stringLength;
     
-    stringLength = response.length();
+    stringLength = userEntry.length();
     
     while (counter < stringLength || stringLength == 0)
     {
-        if (!isalpha(response[counter]))
+        if (!isalpha(userEntry[counter]))
         {
             cout << "Special characters are not allowed. Only numbers and letters. Please try again.\n";
             cout << ">>";
-            getline(cin,response);
-            stringLength = response.length();
+            getline(cin,userEntry);
+            stringLength = userEntry.length();
             counter = 0;
         }
         else
@@ -119,47 +116,60 @@ string validateUserResponse(string userEntry)
             counter++;
         }
     }
-    return response;
     
 }
 
-//function that displays saved user stats
-void displayStats(string displayStatsUser)
+//function that displays saved user stats from file
+void displayStats(string& userName)
 {
-    string userNameStats = displayStatsUser; //pulls from the return value of initMenu
     ifstream statsInfile;
     ofstream statsOutfile;
     string userStatsTitles[3] = {"CORRECT ANSWERS: ", "WRONG ANSWERS: ", "MONEY EARNED: $"};
     int counter = 0;
     string data;
-    //int strLength = userNameStats.length();
+    string correctAnswerIn;
+    string wrongAnswerIn;
+    string moneyEarnedIn;
+    string tempDataIn[3] = {};
+    double moneyEarnedDouble;
     
-    cout << "\n\nCurrent Statistics For " << userNameStats << endl;
-    cout << "***********************************" << endl;
+    cout << "\nCurrent Statistics For " << userName << endl;
+    cout << "**********************************************" << endl;
     
-    statsInfile.open((userNameStats + ".txt").c_str());
+    statsInfile.open((userName + ".txt").c_str());
     while (!statsInfile.eof())
     {
         while(getline(statsInfile,data))
         {
-            cout << userStatsTitles[counter] << data << "\n";
+            tempDataIn[counter] = data;
             counter++;
         }
     }
     statsInfile.close();
-    cout << "\n\n";
+    
+    correctAnswerIn = tempDataIn[0];
+    wrongAnswerIn = tempDataIn[1];
+    moneyEarnedIn = tempDataIn[2];
+    
+    moneyEarnedDouble = stod(moneyEarnedIn);
+    
+    cout << userStatsTitles[0] << correctAnswerIn << "\n" << userStatsTitles[1] << wrongAnswerIn << endl;
+    cout << userStatsTitles[2];
+    cout << fixed << showpoint;
+    cout << setprecision(2);
+    cout << moneyEarnedDouble << endl;
     
 }
 
 //main menu that presents options
-void menu(string menuUser)
+void menu(string& userName)
 {
-    string user = menuUser;
-    cout << user << endl;
-    string selection;
+    string continu;
+    string selection = "";
+    int testSelection;
     int temp = 0;
     
-    while (selection != "6" && temp != 6)
+    while (selection != "6")
     {
         
         cout << "**********************************************" << endl;
@@ -177,28 +187,28 @@ void menu(string menuUser)
         cout << ">>";
         
         getline(cin,selection);
-        validateUserResponse2(selection);
-        temp = stoi(selection);
+        temp = validateUserResponse2(selection);
         
         switch (temp)
         {
             case 1:
-                generateAddition(user);
+                generateAddition(userName);
                 break;
                 
             case 2:
-                generateSubtraction();
+                generateSubtraction(userName);
                 break;
                 
             case 3:
-                generateMultiplication();
+                generateMultiplication(userName);
                 break;
                 
             case 4:
-                generateDivision();
+                generateDivision(userName);
                 break;
+                
             case 5:
-                displayStats(user);
+                displayStats(userName);
                 break;
                 
             case 6:
@@ -207,14 +217,16 @@ void menu(string menuUser)
                 
             default:
                 cout << "Invalid input. No special characters OR SPACES. Try again!\n" << endl;
+                break;
                 
         }
-        
+    
     }
+    
 }
 
 //generates addition problem and answer
-void generateAddition(string userName)
+void generateAddition(string& userName)
 {
     int unsigned seedOne = time(0);
     int number1;
@@ -223,22 +235,21 @@ void generateAddition(string userName)
     int convertedNumber;
     int answer=0;
     bool correct;
-    string temp;
-    string user = userName;
+    int temp;
     
     srand(seedOne);
-    number1 = rand() % 1000;
-    number2 = rand() % 1000;
+    number1 = rand() % 30;
+    number2 = rand() % 30;
     
     answer = number1 + number2;
     
     cout << number1 << " + " << number2 << " = ";
     getline(cin,userNumber);
     
-    temp = validateUserResponse2(userNumber);
-    convertedNumber = atoi(temp.c_str());
     
-    if (convertedNumber == answer)
+    temp = validateUserResponse2(userNumber);
+    
+    if (temp == answer)
     {
         cout << "*****CORRECT!*****\n\n";
         correct = true;
@@ -248,12 +259,12 @@ void generateAddition(string userName)
         cout << "*****INCORRECT!*****\n\n";
         correct = false;
     }
-    updateStats(user,correct);
+    updateStats(userName,correct);
     
 }
 
 //generates division problem and answer
-void generateDivision()
+void generateDivision(string& userName)
 {
     int unsigned seed = time(0);
     int number1=0;
@@ -261,16 +272,14 @@ void generateDivision()
     string userNumber;
     int answer;
     bool correct;
-    string temp;
+    int temp;
     int convertedNumber;
     srand(seed);
     
-    while (number2 == 0 || number2 > number1 || number1%number2 != 0)
+    while (number2 == 0 || number2 > number1 || number1%number2 != 0) //makes sure there is no divide by 0 error and also that answers are only positive integers
     {
-        number1 = rand() % 1000;
-        //cout << number1 << endl; //printing for debugging
-        number2 = rand() % 1000;
-        //cout << number2 << endl; //printing for debugging
+        number1 = rand() % 100;
+        number2 = rand() % 10;
     }
     
     answer = number1/number2;
@@ -279,10 +288,9 @@ void generateDivision()
     getline(cin,userNumber);
     
     temp = validateUserResponse2(userNumber);
-    convertedNumber = atoi(temp.c_str());
 
     cout << endl;
-    if (convertedNumber == answer)
+    if (temp == answer)
     {
         cout << "*****CORRECT!*****\n\n";
         correct = true;
@@ -292,25 +300,25 @@ void generateDivision()
         cout << "*****INCORRECT!*****\n\n";
         correct = false;
     }
+    updateStats(userName,correct);
     
 }
 
 //generates multiplication problem and answer
-void generateMultiplication()
+void generateMultiplication(string& userName)
 {
     int unsigned seedOne = time(0);
-    
     int number1;
     int number2;
     string userNumber;
-    string temp;
+    int temp;
     int answer=0;
     bool correct;
     int convertedNumber;
     
     srand(seedOne);
-    number1 = rand() % 1000;
-    number2 = rand() % 1000;
+    number1 = rand() % 10;
+    number2 = rand() % 10;
     
     answer = number1 * number2;
     
@@ -318,9 +326,8 @@ void generateMultiplication()
     getline(cin,userNumber);
     
     temp = validateUserResponse2(userNumber);
-    convertedNumber = atoi(temp.c_str());
     
-    if (convertedNumber == answer)
+    if (temp == answer)
     {
         cout << "*****CORRECT!*****\n\n";
         correct = true;
@@ -330,27 +337,30 @@ void generateMultiplication()
         cout << "*****INCORRECT!*****\n\n";
         correct = false;
     }
+    updateStats(userName,correct);
     
 }
 
 //generates subtraction problem and answer
-void generateSubtraction()
+void generateSubtraction(string& userName)
 {
     int unsigned seedOne = time(0);
-    
-    int number1;
-    int number2;
+    int number1=0;
+    int number2=0;
     string userNumber;
-    string temp;
+    int temp;
     int answer=0;
     bool correct;
     int convertedNumber;
     
     srand(seedOne);
-    while (number1<number2)
+    number1 = rand() % 100;
+    number2 = rand() % 100;
+    
+    while (number1 < number2)
     {
-        number1 = rand() % 1000;
-        number2 = rand() % 1000;
+        number1 = rand() % 100;
+        number2 = rand() % 100;
     }
     answer = number1 - number2;
     
@@ -358,9 +368,9 @@ void generateSubtraction()
     getline(cin,userNumber);
     
     temp = validateUserResponse2(userNumber);
-    convertedNumber = atoi(temp.c_str());
+
     
-    if (convertedNumber == answer)
+    if (temp == answer)
     {
         cout << "*****CORRECT!*****\n\n";
         correct = true;
@@ -370,18 +380,22 @@ void generateSubtraction()
         cout << "*****INCORRECT!*****\n\n";
         correct = false;
     }
+    updateStats(userName,correct);
     
 }
 
+//function to exit the program
 void quit()
 {
     cout << "Thank you for playing THE MATH GAME! See you soon!" << endl;
+    exit(EXIT_SUCCESS);
 }
 
-//validates user response for different selections
-string validateUserResponse2(string userEntry)
+//validates user response for different selections (for ints only)
+int validateUserResponse2(string userEntry)
 {
     string response = userEntry;
+    int integerResponse = 0;
     int counter = 0;
     int stringLength;
     
@@ -391,8 +405,8 @@ string validateUserResponse2(string userEntry)
     {
         if (!isdigit(response[counter]))
         {
-            cout << "Numbers only. No spaces. Please try again.\n";
-            cout << ">> ";
+            cout << "Numbers only. Choose from selections listed. No spaces. Please try again.\n";
+            cout << ">>";
             getline(cin,response);
             stringLength = response.length();
             counter = 0;
@@ -402,16 +416,69 @@ string validateUserResponse2(string userEntry)
             counter++;
         }
     }
-    return response;
+    integerResponse = stoi(response);
+    return integerResponse;
     
 }
 
-
-void updateStats(string userName, bool adjustStat)
+//function to update user files/stats depending on user's correct answer
+void updateStats(string& userName, bool& status)
 {
-    string user = userName;
-    bool status = adjustStat;
     ifstream updateStatsIn;
     ofstream updateStatsOut;
+    int counter = 0;
+    string data;
+    string infoTemp[3] = {};
+    string correctAnswerIn;
+    string wrongAnswerIn;
+    string moneyEarnedIn;
+    int correctAnswerCalculate;
+    int wrongAnswerCalculate;
+    double moneyEarnedCalculate;
+    string correctAnswerOut;
+    string wrongAnswerOut;
+    string moneyEarnedOut;
     
+    
+    updateStatsIn.open((userName + ".txt").c_str());
+    while (!updateStatsIn.eof())
+    {
+        while(getline(updateStatsIn,data))
+        {
+            infoTemp[counter] = data;
+            counter++;
+        }
+
+    }
+    updateStatsIn.close();
+    
+    correctAnswerIn = infoTemp[0];
+    wrongAnswerIn = infoTemp[1];
+    moneyEarnedIn = infoTemp[2];
+    
+    correctAnswerCalculate = stoi(correctAnswerIn);
+    wrongAnswerCalculate = stoi(wrongAnswerIn);
+    moneyEarnedCalculate = stod(moneyEarnedIn);
+    
+    if (status == true)
+    {
+        correctAnswerCalculate++;
+        moneyEarnedCalculate = moneyEarnedCalculate + ADD_MONEY;
+    }
+    else
+    {
+        wrongAnswerCalculate++;
+        moneyEarnedCalculate = moneyEarnedCalculate - SUBTRACT_MONEY;
+    }
+    
+    correctAnswerOut = to_string(correctAnswerCalculate);
+    wrongAnswerOut = to_string(wrongAnswerCalculate);
+    moneyEarnedOut = to_string(moneyEarnedCalculate);
+    
+    updateStatsOut.open((userName + ".txt").c_str());
+    
+    updateStatsOut << correctAnswerOut << "\n" << wrongAnswerOut << "\n" << moneyEarnedOut << "\n";
+    
+    updateStatsOut.close();
+
 }
